7ce19488e60c53ade1190383cb3b4b90e8b3cd54
Felix Partington <felix@Felixs-Mac-mini.fritz.box>
2021-08-07 13:16:07 +0100

    added onchange event that populates number of people depending on room selection

--- assets/js/meetings.js
+++ assets/js/meetings.js
@@ -30,3 +30,25 @@ function populate (s1, s2) {
           }
       }
   }
+
+// populates choose number of people
+function populateNumbers(s1, s2, s3) {
+  $('#numberPeople option').remove();
+  var s1 = document.getElementById(s1)
+  var s2 = document.getElementById(s2)
+  var location = document.getElementById(s3).value
+  var room = s1.value
+  var maxNumber = (locations[location][room])
+  var placementText = document.createElement("option");
+  if (room != "choose a room..." || "Choose a location...") {
+     placementText.value = ""
+       placementText.innerHTML = "Choose number of people..."
+       s2.options.add(placementText);
+  for (i=1; i<=maxNumber; i++) {
+      var newOption = document.createElement("option");
+          newOption.value = i;
+          newOption.innerHTML = i;
+          s2.options.add(newOption);
+      }
+    }
+  }  
