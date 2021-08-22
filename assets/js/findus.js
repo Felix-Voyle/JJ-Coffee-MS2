3ca982eb75bb9a763d4324440d849eff5dedaa67
Felix Partington <felix@Felixs-Mac-mini.fritz.box>
2021-08-22 10:18:48 +0100

    changed zoom of map out 1 point

--- assets/js/findus.js
+++ assets/js/findus.js
@@ -2,7 +2,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 51.517783, lng: -0.107769},
-    zoom: 15
+    zoom: 14
   });
   const request = {
     placeId: 'ChIJxUdm0LIEdkgRFZIaT5DLJJY',
